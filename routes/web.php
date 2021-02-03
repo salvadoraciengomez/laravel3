fe87afddc904af02a76979525e039f8866aa028d
Salvador Acien <salvadoraciengomez@protonmail.com>
2021-02-03 12:52:15 +0100

    Returning arrays from GET method using routed views && default option

--- routes/web.php
+++ routes/web.php
@@ -63,8 +63,11 @@ Route::get('posts/{post}', function($elPost){
         'entry2'=> 'UYUYUYUYUMigatohace'
     ];
 
+    #Nota7
+    if (! array_key_exists($elPost, $entradas)) abort(404,'No se ha encontrado la clave del array')
+
     return view('posts', [
-        'elPost' => $entradas[$elPost] ?? 'Default option if no key match'
+        'elPost' => $entradas[$elPost] //?? 'Default option if no key match' VER NOTA7
     ]);
 });
 
