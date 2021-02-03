63530623400a47cb7e6155c472390ad7145a4710
Salvador Acien <salvadoraciengomez@protonmail.com>
2021-02-03 11:33:27 +0100

    Restoring route comments

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Solving GitHubCredentials
+
+//NOTA1
+//Default route
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//NOTA2
+//Devuelve el json del array
+//Route::get('/', function () {
+//    return ['myKey' => 'myValue'];
+//});
+
+
+//NOTA3
+//Llama al resources/views/test.blade.php
+// Route::get('test', function(){
+//     return view('test');
+// });
+
+//NOTA4
+//Muestra la variable recogida en la request GET http://localhost:8080/?variable=hola
+// Route::get('/', function(){
+//     $getVar=request('variable');
+//     return $getVar;
+// });
+
+//NOTA5
+//Route::get('/', function () {
+//    $variable = request('variable');
+//    return view('test',[
+//        'variable' => $variable
+        //segundo parámetro del view() es un array al que se le pasa en la clave el nombre que la vista cargará desde dicha variable local 
+        //hacia la nueva variable que la vista (valor de la clave) cargará entre la etiqueta <?= $variable
+        //Para enviar el valor hacia la vista, sin el array, solamente haría falta usar <?= {{ $variable }}
+        //Inyección JS desde GET http://localhost:8080/?variable=<script>alert("does")</script>
+//    ]);
+//});
+
+
